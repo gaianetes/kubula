1b2fe11d980ad77ef6c8e6ba6bf9fa0276158d8c
Mitch Murphy <mitch.murphy@gmail.com>
2024-03-26 08:31:08 -0400

    Resolved all ansible-lint errors

--- infrastructure/packer/scripts/cleanup.sh
+++ infrastructure/packer/scripts/cleanup.sh
@@ -1,12 +1,33 @@
-#!/bin/bash -eux
+#!/bin/sh
 
-pip3 uninstall -y ansible
+pip3 uninstall ansible -y
+dnf -y clean all
 
-dnf clean all
+# Remove Virtualbox specific files
 
-# Zero out the rest of the free space using dd, then delete the written file.
+rm -rf /usr/src/vboxguest* /usr/src/virtualbox-ose-guest*
+rm -rf *.iso *.iso.? /tmp/vbox /home/vagrant/.vbox_version
+
+# Cleanup log files
+find /var/log -type f | while read f; do echo -ne '' > $f; done;
+
+# remove under tmp directory
+rm -rf /tmp/*
+
+# remove interface persistent
+rm -f /etc/udev/rules.d/70-persistent-net.rules
+
+for ifcfg in $(ls /etc/sysconfig/network-scripts/ifcfg-*)
+do
+    if [ "$(basename ${ifcfg})" != "ifcfg-lo" ]
+    then
+        sed -i '/^UUID/d'   /etc/sysconfig/network-scripts/ifcfg-enp0s3
+        sed -i '/^HWADDR/d' /etc/sysconfig/network-scripts/ifcfg-enp0s3
+    fi
+done
+
 dd if=/dev/zero of=/EMPTY bs=1M
-rm -f /EMPTY
+rm -rf /EMPTY
 
 # Add `sync` so Packer doesn't quit too early, before the large file is deleted.
-sync
+# sync
