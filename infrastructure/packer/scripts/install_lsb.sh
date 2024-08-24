e7080cc8729e50e82dabe688359e1270f6f9bd3a
Mitchell Murphy <mitchell.murphy@darkwolfsolutions.com>
2024-08-24 16:00:54 -0400

    Updated Ansible playbooks for full rke2 and cilium install

---
+++ infrastructure/packer/scripts/install_lsb.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# update sudoers.d to not ask for password for rke2 user
+sudo echo "rke2 ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/rke2
+# reload rke2 user
+sudo su - rke2
+
+# need to install lsb for ansible to work
+sudo dnf install -y yum-utils
+sudo dnf config-manager --set-enabled devel
+sudo dnf update -y
+sudo dnf install redhat-lsb-core
