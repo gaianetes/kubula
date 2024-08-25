e56ae8adb414bb79f5de648caf4dbf5be96d3f30
Mitchell Murphy <mitchell.murphy@darkwolfsolutions.com>
2024-08-25 19:03:57 -0400

    Wrote and tested Ansible playbooks to configure multiple RKE2 clusters with Cilium Mesh

--- infrastructure/packer/scripts/install_lsb.sh
+++ infrastructure/packer/scripts/install_lsb.sh
@@ -2,11 +2,9 @@
 
 # update sudoers.d to not ask for password for rke2 user
 sudo echo "rke2 ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/rke2
-# reload rke2 user
-sudo su - rke2
 
 # need to install lsb for ansible to work
 sudo dnf install -y yum-utils
 sudo dnf config-manager --set-enabled devel
 sudo dnf update -y
-sudo dnf install redhat-lsb-core
+sudo dnf install -y redhat-lsb-core
